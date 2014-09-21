12b6a11042e8898f32b20db50b6cca86ce15ad3a
xiaopeng.fxp <xiaopeng.fxp@alibaba-inc.com>
2014-09-21 14:56:07 +0800

    update robo

--- Demo/src/com/soper/demo/update/updateFragment.java
+++ Demo/src/com/soper/demo/update/updateFragment.java
@@ -1,9 +1,9 @@
 package com.soper.demo.update;
 
+import roboguice.fragment.RoboFragment;
+import roboguice.inject.InjectView;
 import android.app.Activity;
-import android.content.Context;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -12,40 +12,42 @@ import android.widget.Button;
 
 import com.soper.demo.R;
 import com.soper.demo.myAppliction.MyApplication;
-import com.umeng.fb.FeedbackAgent;
 import com.umeng.update.UmengUpdateAgent;
 
-public class updateFragment extends Fragment {
+public class updateFragment extends RoboFragment {
 
-	private Button updateButton;
-	private Context mContext;
+	@InjectView(R.id.update_button)
+	Button updateButton;
 
 	@Override
 	public void onAttach(Activity activity) {
 		// TODO Auto-generated method stub
 		super.onAttach(activity);
-		this.mContext = activity;
 	}
-	
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		View rootView = inflater.inflate(R.layout.fragment_update, container,
 				false);
-		
-		updateButton = (Button) rootView.findViewById(R.id.update_button);
+
+		return rootView;
+
+	}
+
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onActivityCreated(savedInstanceState);
 		updateButton.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
 
-				 UmengUpdateAgent.forceUpdate(MyApplication.context);
-				
-				
+				UmengUpdateAgent.forceUpdate(MyApplication.context);
+
 			}
 		});
-		return rootView;
-
 	}
 
 }
